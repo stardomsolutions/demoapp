7367c04d799dbfff548ab1714b9b02c48e7eb13c
“KSR <ksrconsulating@gmail.com>
2022-04-07 07:10:27 +0530

    changed the api response

--- src/main/java/com/pradheera/demoapp/App.java
+++ src/main/java/com/pradheera/demoapp/App.java
@@ -3,6 +3,6 @@ package com.pradheera.demoapp;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        System.out.println("Hello World, Welcome to DevOps Session ");
     }
 }
